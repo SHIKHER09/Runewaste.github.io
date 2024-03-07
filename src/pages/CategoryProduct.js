e73d7d94d519e4450177c9a3fcce1d4534c7d040
Shikher Kumar <75657175+SHIKHER09@users.noreply.github.com>
2024-03-07 13:29:16 +0530

    Update CategoryProduct.js

--- src/pages/CategoryProduct.js
+++ src/pages/CategoryProduct.js
@@ -1,100 +1,127 @@
 import React, { useState, useEffect } from "react";
-import Layout from "../components/Layout/Layout";
-import { useParams, useNavigate } from "react-router-dom";
-import "../styles/CategoryProductStyles.css";
+import Layout from "./../components/Layout/Layout";
 import axios from "axios";
-const CategoryProduct = () => {
+import { useParams, useNavigate } from "react-router-dom";
+import "../styles/ProductDetailsStyles.css";
+
+const ProductDetails = () => {
   const params = useParams();
   const navigate = useNavigate();
-  const [products, setProducts] = useState([]);
-  const [category, setCategory] = useState([]);
+  const [product, setProduct] = useState({});
+  const [relatedProducts, setRelatedProducts] = useState([]);
 
+  //initalp details
   useEffect(() => {
-    if (params?.slug) getPrductsByCat();
+    if (params?.slug) getProduct();
   }, [params?.slug]);
-  const getPrductsByCat = async () => {
+  //getProduct
+  const getProduct = async () => {
     try {
       const { data } = await axios.get(
-        `/api/v1/product/product-category/${params.slug}`
+        `https://runewaste-backend-1.onrender.com/api/v1/product/get-product/${params.slug}`
       );
-      setProducts(data?.products);
-      setCategory(data?.category);
+      setProduct(data?.product);
+      getSimilarProduct(data?.product._id, data?.product.category._id);
     } catch (error) {
       console.log(error);
     }
   };
-
+  //get similar product
+  const getSimilarProduct = async (pid, cid) => {
+    try {
+      const { data } = await axios.get(
+        `https://runewaste-backend-1.onrender.com/api/v1/product/related-product/${pid}/${cid}`
+      );
+      setRelatedProducts(data?.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Layout>
-      <div className="container mt-3 category">
-        <h4 className="text-center">Category - {category?.name}</h4>
-        <h6 className="text-center">{products?.length} result found </h6>
-        <div className="row">
-          <div className="col-md-9 offset-1">
-            <div className="d-flex flex-wrap">
-              {products?.map((p) => (
-                <div className="card m-2" key={p._id}>
-                  <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
-                    className="card-img-top"
-                    alt={p.name}
-                  />
-                  <div className="card-body">
-                    <div className="card-name-price">
-                      <h5 className="card-title">{p.name}</h5>
-                      <h5 className="card-title card-price">
-                        {p.price.toLocaleString("en-US", {
-                          style: "currency",
-                          currency: "USD",
-                        })}
-                      </h5>
-                    </div>
-                    <p className="card-text ">
-                      {p.description.substring(0, 60)}...
-                    </p>
-                    <div className="card-name-price">
-                      <button
-                        className="btn btn-info ms-1"
-                        onClick={() => navigate(`/product/${p.slug}`)}
-                      >
-                        More Details
-                      </button>
-                      {/* <button
-                    className="btn btn-dark ms-1"
-                    onClick={() => {
-                      setCart([...cart, p]);
-                      localStorage.setItem(
-                        "cart",
-                        JSON.stringify([...cart, p])
-                      );
-                      toast.success("Item Added to cart");
-                    }}
-                  >
-                    ADD TO CART
-                  </button> */}
-                    </div>
-                  </div>
+      <div className="row container product-details">
+        <div className="col-md-6">
+          <img
+            src={`https://runewaste-backend-1.onrender.com/api/v1/product/product-photo/${product._id}`}
+            className="card-img-top"
+            alt={product.name}
+            height="300"
+            width={"350px"}
+          />
+        </div>
+        <div className="col-md-6 product-details-info">
+          <h1 className="text-center">Product Details</h1>
+          <hr />
+          <h6>Name : {product.name}</h6>
+          <h6>Description : {product.description}</h6>
+          <h6>
+            Price :
+            {product?.price?.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </h6>
+          <h6>Category : {product?.category?.name}</h6>
+          <button class="btn btn-secondary ms-1">ADD TO CART</button>
+        </div>
+      </div>
+      <hr />
+      <div className="row container similar-products">
+        <h4>Similar Products ➡️</h4>
+        {relatedProducts.length < 1 && (
+          <p className="text-center">No Similar Products found</p>
+        )}
+        <div className="d-flex flex-wrap">
+          {relatedProducts?.map((p) => (
+            <div className="card m-2" key={p._id}>
+              <img
+                src={`/api/v1/product/product-photo/${p._id}`}
+                className="card-img-top"
+                alt={p.name}
+              />
+              <div className="card-body">
+                <div className="card-name-price">
+                  <h5 className="card-title">{p.name}</h5>
+                  <h5 className="card-title card-price">
+                    {p.price.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </h5>
                 </div>
-              ))}
+                <p className="card-text ">
+                  {p.description.substring(0, 60)}...
+                </p>
+                <div className="card-name-price">
+                  <button
+                    className="btn btn-info ms-1"
+                    style={{ backgroundColor: " #e22454" }}
+                    onClick={() => navigate(`/product/${p.slug}`)}
+                    style={{ backgroundColor: " #e22454" }}
+                  >
+                    More Details
+                  </button>
+                  {/* <button
+                  className="btn btn-dark ms-1"
+                  onClick={() => {
+                    setCart([...cart, p]);
+                    localStorage.setItem(
+                      "cart",
+                      JSON.stringify([...cart, p])
+                    );
+                    toast.success("Item Added to cart");
+                  }}
+                >
+                  ADD TO CART
+                </button> */}
+                </div>
+              </div>
             </div>
-            {/* <div className="m-2 p-3">
-            {products && products.length < total && (
-              <button
-                className="btn btn-warning"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setPage(page + 1);
-                }}
-              >
-                {loading ? "Loading ..." : "Loadmore"}
-              </button>
-            )}
-          </div> */}
-          </div>
+          ))}
         </div>
       </div>
     </Layout>
   );
 };
 
-export default CategoryProduct;
+export default ProductDetails;
